0e2de13dcb8fadb96d612fc6a02744f1f087ec6d
Lorenzo Gamboa García <lorenzogamboagarcia@gmail.com>
2019-02-05 12:35:51 +0100

    Update auth.js

--- src/chat/auth.js
+++ src/chat/auth.js
@@ -2,16 +2,16 @@ module.exports = {
     'facebookAuth': {
         'clientID': 'your-secret-clientID-here',
         'clientSecret': 'your-client-secret-here',
-        'callbackURL': '/auth/facebook/callback'
+        'callbackURL': ''***********'
     },
     'googleAuth': {
         'clientID': '951407658510-bc0hd6ejdcvgqt16ma483cl0ses217l2.apps.googleusercontent.com',
-        'clientSecret': 'Z4Y-D0NAfU7ns3vX8cj8t_e6',
+        'clientSecret': ''***********',
         'callbackURL': '/auth/google/callback'
     },
     'nodemailerAuth': {
         'SERVICE': 'gmail',
         'USER': 'lorenzogamboagarcia@gmail.com',
-        'PASSWORD': 'submarinoperal'
+        'PASSWORD': '***********
     }
 };
